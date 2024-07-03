712ce82204121f535ec6d19115a1f00dcfc1aa41
sasi-516 <bsasisss145@gmail.com>
2024-07-03 15:40:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Hi ra gandhus.
+          Edit <code>src/App.js</code> Hi sasi welcome.
         </p>
         <a
           className="App-link"
